1f2f4c7202af8a8b8a48ec472d4086702805380e
Farzana Karim <farzana.mkarim@gmail.com>
2020-07-24 23:52:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
   row_index = 0
 
   while row_index < src.count do
-    element_index = 0 
+    element_index = 0
     while element_index < src.count[row_index].count do
       if src[row_index][element_index].count == Strings
         just_the_strings << src[row_index][element_index]
